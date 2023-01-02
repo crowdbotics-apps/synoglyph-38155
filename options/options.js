bb1a02f427ac866fdfb51c4d6aa2fc4dd237640c
Crowdbotics <team@crowdbotics.com>
2023-01-02 21:16:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "synoglyph_38155",
+  url: "https://synoglyph_38155.botics.co",
+  api: "https://synoglyph_38155.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
